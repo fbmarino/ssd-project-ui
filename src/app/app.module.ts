e21548206aa7e756c48d417abea65570e13d800e
Fabrizio Marino <fb.marino@hotmail.it>
2022-11-23 22:39:25 +0100

    *

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
-import { AppToolbarComponent } from './shared/app-toolbar/app-toolbar.component';
-import { HomeComponent } from './pages/home/home.component';
+import {AppToolbarComponent} from './shared/app-toolbar/app-toolbar.component';
+import {HomeComponent} from './pages/home/home.component';
 import {MatCardModule} from "@angular/material/card";
-import {MatGridListModule} from "@angular/material/grid-list";
 
 @NgModule({
   declarations: [
@@ -26,9 +25,9 @@ import {MatGridListModule} from "@angular/material/grid-list";
     MatIconModule,
     MatButtonModule,
     MatCardModule,
-    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
