5d6b4b70aee02d6581a34767d3252f33d17f6908
Fabrizio Marino <fb.marino@hotmail.it>
2022-11-27 20:10:15 +0100

    fixed toolbar avatar

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -13,15 +13,34 @@ export class AvatarComponent implements OnInit {
   sizeClass: string = '';
   colorClass: string = '';
 
+  private colorMaxNum = 6;
+
   ngOnInit() {
-    if (this.name.length >= 2)  {
+    this.updateLabelAndColor();
+    this.sizeClass = 'size-' + this.size;
+  }
+
+  ngOnChanges() {
+    this.updateLabelAndColor();
+  }
+
+  updateLabelAndColor() {
+    const words = this.name.split(' ');
+    if (words.length >= 2)  {
+      this.label = words[0][0].toUpperCase() + words[1][0].toUpperCase();
+    } else if (this.name.length >= 2)  {
       this.label = this.name[0].toUpperCase() + this.name[1].toUpperCase();
     } else if (this.name.length >= 1)  {
       this.label = this.name[0].toUpperCase();
     } else {
       this.label = 'A';
     }
-    this.colorClass = 'c-4';
-    this.sizeClass = 'size-' + this.size;
+
+    let colorNum = 0;
+    for (let i = 0; i < this.label.length; i++) {
+      colorNum = this.label.charCodeAt(i) + ((colorNum << 5) - colorNum);
+    }
+    colorNum = colorNum % this.colorMaxNum + 1;
+    this.colorClass = 'c-' + colorNum;
   }
 }
