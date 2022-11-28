47619061122e161c9721a1745901e45315ef617b
Fabrizio Marino <fb.marino@hotmail.it>
2022-11-28 12:27:01 +0100

    removed login page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
-import {LoginDialogComponent} from "./dialogs/login/login-dialog.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'login',
-    component: LoginDialogComponent
-  },
+  }
 ];
 
 @NgModule({
